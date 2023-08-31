9b83f9ae661d19ab9ee5f5e7be8f0eb098de9681
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-09-01 01:23:28 +0530

    fix: fix eslint errors

--- src/app/api/payment/confirm/route.ts
+++ src/app/api/payment/confirm/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export function POST(request: Request) {
   console.log("Request Received");
   console.log(JSON.stringify(request.body));
   NextResponse.json({ status: "done" });
