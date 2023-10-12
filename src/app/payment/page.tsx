9e18a0e4df5157e1c4d50ffdb5afcc2ec59f3985
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2023-10-13 00:43:29 +0530

    minor chagnes

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 
-export default function PaymentConfirmationPage({
-  params,
-}: {
-  params: { status: "success" | "failed" };
-}) {
+export default function PaymentConfirmationPage() {
   const searchParams = useSearchParams();
 
   return (
     <div className="mx-auto my-auto grid h-[600px] w-[min(100%,1000px)] place-content-center rounded-lg bg-slate-50 p-12 shadow-lg shadow-black/10">
-      {params.status === "success" ? (
+      {searchParams.get("status") === "success" &&
+      searchParams.get("status_code") === "0" ? (
         <div className="flex flex-col items-center justify-center gap-y-9">
           <h1 className="text-center text-5xl font-bold tracking-tight text-slate-700">
             Transation successful!
           </h1>
           <p className="text-center text-lg text-black">{`Order Id: ${searchParams.get(
             "order_id"
+          )}`}</p>
+          <p className="text-center text-lg text-black">{`Order Ref: ${searchParams.get(
+            "order_ref"
+          )}`}</p>
+
+          <p className="text-center text-lg text-black">{`Transction Timestamp: ${searchParams.get(
+            "transction_time"
+          )}`}</p>
+
+          <p className="text-center text-lg text-black">{`Amount: LKR ${searchParams.get(
+            "amount"
           )}`}</p>
         </div>
       ) : (
